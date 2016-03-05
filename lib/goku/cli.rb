c041ebd7435c77bfdef2b34253b7b70760e5c83c
Igor Šarčević <igor@renderedtext.com>
2016-03-05 21:30:14 +0100

    Bump version

--- lib/goku/cli.rb
+++ lib/goku/cli.rb
@@ -5,16 +5,16 @@ module Goku
     def c(raw_path)
       path = Goku::Path.new(raw_path)
 
-      puts path.directories
-      puts path.filename
+      puts path.full
+      puts path.to_spec.full
     end
 
     desc "m PATH", "Create a module"
     def m(path)
       path = Goku::Path.new(raw_path)
 
-      puts path.directories
-      puts path.filename
+      puts path.full
+      puts path.to_spec.full
     end
 
     map "module" => "m"
